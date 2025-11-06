da937f6fda2dd1dd6e1753b1249370c8d4aeba44
dharak rana <dharakrana65@gmail.com>
2025-11-07 00:00:52 +0530

    right rotation

--- Array/arr-rotation.js
+++ Array/arr-rotation.js
@@ -5,7 +5,7 @@ const arr = [1,2,3,4,5,6,7]
  * @param {} arr 
  * @param {*} k 
  */
-function rotateByK(arr,rotations) {
+function rotateLeftByK(arr,rotations) {
     let k = rotations % arr.length; // make rotation minimum
     let temp = [];
     for(let i = 0; i < k; i++) {
@@ -21,5 +21,21 @@ function rotateByK(arr,rotations) {
     }
 }
 
-rotateByK(arr, 3)
+function rotateRightByK(arr, rotations) {
+    let k = rotations % arr.length; // make rotation minimum
+    let temp = [];
+    for(let i = arr.length - k; i < arr.length; i++) {
+        temp.push(arr[i]);
+    }
+
+    for(let i = arr.length - k - 1; i >= 0; i--) {
+        arr[i + k] = arr[i];
+    }
+
+    for(let i = 0; i < k; i++) {
+        arr[i] = temp[i];
+    }
+}
+rotateLeftByK(arr, 3)
+
 console.log(arr)
